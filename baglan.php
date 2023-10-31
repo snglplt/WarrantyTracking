839c227fae873fea8832c7a361cf3c0c5f1197ce
Songul Polat <snglplt.36@gmail.com>
2023-10-31 17:54:34 +0300

    change baglan test 2

--- baglan.php
+++ baglan.php
@@ -7,7 +7,7 @@ session_start();
 $username="******";
 $password="*****";
 $sunucu="localhost";
-$database="admin_pratik";
+$database="******";
 date_default_timezone_set('Europe/Istanbul');
 $baglan=mysqli_connect($sunucu,$username,$password);
 mysqli_query($baglan,"SET NAMES UTF8");
